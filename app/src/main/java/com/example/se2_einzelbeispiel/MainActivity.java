9748ec63c3a2b5a1a49615b1fab64c5cdee99079
Max Krassnitzer <m1krassnit@edu.aau.at>
2023-03-11 19:40:49 +0100

    step 1 done (tcp connection + display calculation)

---
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.se2_einzelbeispiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextInput = findViewById(R.id.editText_input);
+                new tcpConnection(editTextInput.getText().toString()).execute();
+            }
+        });
+    }
+    public class tcpConnection extends AsyncTask<Void, Void, String> {
+
+        private final String host = "se2-isys.aau.at";
+        private final int port = 53212;
+
+        private String input;
+
+        public tcpConnection(String input) {
+            this.input = input;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result = "";
+            try {
+                Socket socket = new Socket(host, port);
+                PrintWriter outputStream = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                outputStream.write(input + "\n");
+                outputStream.flush();
+                result = inputStream.readLine();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String output) {
+            TextView textView = findViewById(R.id.textView_answer);
+            textView.setText(output);
+        }
+    }
+}
+
+
+
+
